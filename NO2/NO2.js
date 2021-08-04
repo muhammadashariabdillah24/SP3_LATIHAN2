0cec7d3960b4cc70d55e392741afd76e17a3ee22
muhammadashariabdillah24 <tutordunet09@gmail.com>
2021-08-04 21:04:55 +0700

    SP3_Latihan2

---
+++ NO2/NO2.js
@@ -0,0 +1,18 @@
+let panggilFilter = () => {
+
+    panggilFill2()
+}
+
+
+
+let panggilFill2 = () => {
+    var kodeUnix = [23, 89,67,29,192,6,2,129,21,872,891,901,70,61,78,62,32,90,90];
+    console.log(
+        kodeUnix.filter(a => a >= 60)
+    );
+    
+}
+
+
+
+panggilFilter(panggilFill2)
---
+++ NO3/NO3.js
@@ -0,0 +1,13 @@
+panggilMap = () => {
+
+    panggilMap2()
+}
+
+panggilMap2 = () => {
+    var kodeUnix = [23, 89,67,29,192,6,2,129,21,872,891,901,70,61,78,62,32,90,90];
+    console.log(
+        kodeUnix.map(a => a * 2 / 5 - kodeUnix.length )
+    );
+}
+
+panggilMap2()
